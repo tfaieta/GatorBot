17f3872b044142a94c94c4ed5c8bd53e27178170
Tony Faieta <tfaieta@gmail.com>
2018-12-19 10:02:49 -0500

    writing some webdriver stuff for reddit

--- scraper.py
+++ scraper.py
@@ -1,16 +1,57 @@
 import time
+import pathlib
 from selenium import webdriver
+
+
+def scrapeReddit(url):
+    driver = webdriver.Firefox()
+    driver.get(url)
+    SCROLL_PAUSE_TIME = 4.0
+
+    # Get tag names
+    redditPostTitles = driver.find_elements_by_tag_name("H2")
+    redditPostParagraphs = driver.find_elements_by_tag_name("P")
+
+    # Get scroll height
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+        # Scroll down to bottom
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        # Wait to load page
+        time.sleep(SCROLL_PAUSE_TIME)
+
+        # Calculate new scroll height and compare with last scroll height
+        new_height = driver.execute_script("return document.body.scrollHeight")
+
+        if new_height == last_height:
+            break
+
+        last_height = new_height
+
+        for redditPostTitle in redditPostTitles:
+            print(redditPostTitle.text)
+
+        for redditPostParagraph in redditPostParagraphs:
+            print(redditPostParagraph.text)
+
+
+# MAIN
 
 # The URL we want to browse to
 urls = [
     "https://www.reddit.com/r/ufl",
-    "https://www.facebook.com/groups/124944151183714/"
+    # "https://www.facebook.com/groups/124944151183714/"
 ]
 
-# Loop through URLS
+reddit_data_folder = "./reddit_data/"
+pathlib.Path(reddit_data_folder).mkdir(parents=True, exist_ok=True)
+
 for url in urls:
-    driver = webdriver.Firefox()
-    driver.get(url)
-    driver.execute_script("window.scrollTo(0,1000);")
-    time.sleep(5)
+    scrapeReddit(url)
+
+
+
+
 
